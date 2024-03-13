6bc166e4189ad798382060f120cba16813756c04
Husejin Hodžić <husejin.hodzic@dccs.eu>
2024-03-13 18:13:40 +0100

    cleaned up secrets from master

--- test-history-cleanup/src/main/java/org/example/ExampleResource.java
+++ test-history-cleanup/src/main/java/org/example/ExampleResource.java
@@ -18,6 +18,6 @@ public class ExampleResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("secret")
     public String secret() {
-        return "***REMOVED***";
+        return "CLEANED UP";
     }
 }
